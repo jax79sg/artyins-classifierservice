565a428dbb15aead48f3699c6f6ea317a399488b
jax79sg <jax79sg>
2019-12-27 14:21:23 +0700

    added unit test

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestModels(unittest.TestCase):
+
+    def test_testmodel(self):
+        from score.testmodel import IrisSVCModel
+        mymodel=IrisSVCModel()
+        
+
+    def test_modifiedtopicmodel(self):
+        pass #Wei Deng to insert
+
+    def test_bertmodel(self):
+        pass #Kah Siong to insert
+
+if __name__ == '__main__':
+    unittest.main()
