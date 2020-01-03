16d84bdddad4e9ec360bf1ad1574b6f3d34a1747
jax79sg <jax79sg@yahoo.com.sg>
2020-01-03 23:28:49 +0800

    Web service to accept prediction calls

---
+++ config.py
@@ -0,0 +1,9 @@
+class InferenceConfig():
+    GPU_COUNT = 1
+    
+    MODEL_SAMPLE_INPUT=dict(sepal_length=1.0,sepal_width=2.2,petal_length=3.3,petal_width=4.4)
+    MODEL_MODULE="score.testmodel"
+    MODEL_CLASS="IrisSVCModel"
+    MODEL_DIR = "model_files"
+    MODEL_FILE = "svc_iris_model.pickle"
+
