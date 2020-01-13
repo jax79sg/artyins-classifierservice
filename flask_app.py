3cc199857655485f5e223b6fe2ca104caff32491
jax79sg <jax79sg@yahoo.com.sg>
2020-01-13 14:33:11 +0800

    fix flask bugs

--- flask_app.py
+++ flask_app.py
@@ -23,7 +23,7 @@ ROOT_DIR = os.path.abspath("../")
 sys.path.append(ROOT_DIR)  # To find local version of the library
 
 # Logging confg
-logging.basicConfig(level=logging.DEBUG,hanndlers=[
+logging.basicConfig(level=logging.DEBUG,handlers=[
         logging.FileHandler("{0}/{1}.log".format(".", "log")),
         logging.StreamHandler()
     ], 
